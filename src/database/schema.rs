fd744fc9c48080e58762e1efee5410e990e11ace
Marshall Bowers <elliott.codes@gmail.com>
2018-12-16 16:32:11 -0500

    Update database schema

--- src/database/schema.rs
+++ src/database/schema.rs
@@ -1,20 +1,29 @@
 table! {
     event_entry (id) {
         id -> Uuid,
+        created_at -> Timestamptz,
+        updated_at -> Timestamptz,
         ledger_id -> Uuid,
         name -> Text,
         timestamp -> Timestamptz,
-        created_at -> Timestamptz,
-        updated_at -> Timestamptz,
     }
 }
 
 table! {
     ledger (id) {
         id -> Uuid,
-        name -> Text,
         created_at -> Timestamptz,
         updated_at -> Timestamptz,
+        name -> Text,
+    }
+}
+
+table! {
+    project (id) {
+        id -> Uuid,
+        created_at -> Timestamptz,
+        updated_at -> Timestamptz,
+        name -> Text,
     }
 }
 
@@ -23,4 +32,5 @@ joinable!(event_entry -> ledger (ledger_id));
 allow_tables_to_appear_in_same_query!(
     event_entry,
     ledger,
+    project,
 );
