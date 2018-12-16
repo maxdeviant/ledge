759bee6ddd7d0ca4c8ebc9f0c78d319ae4711c49
Marshall Bowers <elliott.codes@gmail.com>
2018-12-16 16:18:36 -0500

    Add event entry database table

--- src/database/schema.rs
+++ src/database/schema.rs
@@ -1,4 +1,15 @@
 table! {
+    event_entry (id) {
+        id -> Uuid,
+        ledger_id -> Uuid,
+        name -> Text,
+        timestamp -> Timestamptz,
+        created_at -> Timestamptz,
+        updated_at -> Timestamptz,
+    }
+}
+
+table! {
     ledger (id) {
         id -> Uuid,
         name -> Text,
@@ -6,3 +17,10 @@ table! {
         updated_at -> Timestamptz,
     }
 }
+
+joinable!(event_entry -> ledger (ledger_id));
+
+allow_tables_to_appear_in_same_query!(
+    event_entry,
+    ledger,
+);
