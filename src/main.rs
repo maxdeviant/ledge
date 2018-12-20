33757cd2d1e3afb92cadfde47b75711fe330a825
Marshall Bowers <elliott.codes@gmail.com>
2018-12-19 21:13:37 -0500

    Add basic CLI

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use std::fs::File;
 use std::io::prelude::*;
 
 use chrono::prelude::*;
+use clap::{App, Arg};
 use dotenv::dotenv;
 use serde_derive::{Deserialize, Serialize};
 
@@ -20,53 +21,19 @@ pub struct Log {
 fn main() {
     dotenv().ok();
 
-    let scribe_id = ProjectId::new();
-    let hitheryon_id = ProjectId::new();
+    let matches = App::new("scribe")
+        .version("0.1.0")
+        .author("Marshall Bowers <elliott.codes@gmail.com>")
+        .arg(
+            Arg::with_name("config")
+                .short("c")
+                .long("config")
+                .value_name("FILE")
+                .takes_value(true),
+        )
+        .get_matches();
 
-    let log = Log {
-        projects: vec![
-            Project {
-                id: scribe_id,
-                name: "scribe".into(),
-                status: ProjectStatus::InProgress,
-                started_at: Utc.ymd(2018, 12, 16).and_hms(12, 0, 0),
-            },
-            Project {
-                id: hitheryon_id,
-                name: "Hitheryon".into(),
-                status: ProjectStatus::InProgress,
-                started_at: Utc.ymd(2018, 8, 25).and_hms(15, 34, 27),
-            },
-        ],
-        entries: vec![
-            Entry {
-                id: EntryId::new(),
-                kind: EntryKind::ProjectHeadway(ProjectHeadwayEntry {
-                    project_id: scribe_id,
-                    hours_spent: 4.0,
-                }),
-                timestamp: Utc.ymd(2018, 12, 16).and_hms(12, 0, 0),
-            },
-            Entry {
-                id: EntryId::new(),
-                kind: EntryKind::ProjectHeadway(ProjectHeadwayEntry {
-                    project_id: hitheryon_id,
-                    hours_spent: 10.0,
-                }),
-                timestamp: Utc.ymd(2018, 8, 27).and_hms(3, 41, 29),
-            },
-            Entry {
-                id: EntryId::new(),
-                kind: EntryKind::ProjectHeadway(ProjectHeadwayEntry {
-                    project_id: hitheryon_id,
-                    hours_spent: 1.5,
-                }),
-                timestamp: Utc.ymd(2018, 8, 25).and_hms(16, 56, 41),
-            },
-        ],
-    };
+    let config = matches.value_of("config").unwrap_or(".scribe");
 
-    let mut file = File::create("stats.yaml").unwrap();
-    file.write_all(&serde_yaml::to_string(&log).unwrap().into_bytes())
-        .unwrap();
+    println!("{}", config);
 }
