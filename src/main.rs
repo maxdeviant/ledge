bdd0faf6f353a8c865cd69c69c4b1e5f3126bd98
Marshall Bowers <elliott.codes@gmail.com>
2018-12-19 22:09:50 -0500

    Add ability to add new projects

--- src/main.rs
+++ src/main.rs
@@ -25,6 +25,7 @@ pub struct Log {
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Config {
     pub root_dir: PathBuf,
+    pub log_file: String,
 }
 
 fn main() -> std::io::Result<()> {
@@ -53,18 +54,41 @@ fn main() -> std::io::Result<()> {
 
     let config_path = matches
         .value_of("config")
-        .unwrap_or(default_config_path.to_str().unwrap());
+        .unwrap_or(default_config_path.to_str().expect("No config file"));
 
     let mut config_file = File::open(config_path)?;
     let mut contents = String::new();
     config_file.read_to_string(&mut contents)?;
 
-    let config: Config = toml::from_str(&contents).unwrap();
+    let config: Config = toml::from_str(&contents).expect("Failed to parse config file");
 
     println!("config: {:?}", config);
 
     match matches.subcommand() {
-        ("project", Some(project_matches)) => println!("Project subcommand was used."),
+        ("project", Some(project_matches)) => match project_matches.subcommand() {
+            ("add", Some(add_matches)) => {
+                let project_name = add_matches.value_of("name").expect("No project name");
+
+                let mut log_file = File::open(config.root_dir.join(config.log_file.clone()))?;
+                let mut contents = String::new();
+                log_file.read_to_string(&mut contents)?;
+
+                let mut log: Log =
+                    serde_yaml::from_str(&contents).expect("Failed to parse log file");
+
+                log.projects.push(Project {
+                    id: ProjectId::new(),
+                    name: project_name.to_string(),
+                    status: ProjectStatus::InProgress,
+                    started_at: Utc::now(),
+                });
+
+                let mut log_file = File::create(config.root_dir.join(config.log_file))?;
+                log_file.write_all(&serde_yaml::to_string(&log).unwrap().into_bytes())?;
+            }
+            ("", None) => (),
+            _ => unreachable!(),
+        },
         ("", None) => (),
         _ => unreachable!(),
     }
